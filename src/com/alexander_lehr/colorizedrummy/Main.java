25f1378dad72837c2c15f065da070fa1ff574e29
alexanderlehr <marakanerlp@gmail.com>
2020-11-17 17:55:17 +0100

    finish

--- src/com/alexander_lehr/colorizedrummy/Main.java
+++ src/com/alexander_lehr/colorizedrummy/Main.java
@@ -10,55 +10,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int count = 0;
-
-        for(char c : args[0].toCharArray()) {
-            if(!(c == 'r' || c == 'y' || c == 'b' || c == 'k')) return;
-            chars.put(count, c);
-            count++;
+        for (char c : args[0].toCharArray()) {
+            if (!(c == 'r' || c == 'y' || c == 'b' || c == 'k')) return;
+            chars.put(chars.size(), c);
         }
 
-        count = 0;
+        lists.put(0, new ArrayList<>());
+        lists.get(0).add(chars.get(0));
+        chars.remove(0);
 
-        while (chars.size() > 0) {
-            for(int i : chars.keySet()) {
-                if(lists.get(0) == null) {
-                    lists.put(count, new ArrayList<>());
-                    lists.get(count).add(chars.get(i));
+        for (int i : chars.keySet()) {
+            for (int j : lists.keySet()) {
+                if (!lists.get(j).contains(chars.get(i)) && lists.get(j).size() < 3) {
+                    lists.get(j).add(chars.get(i));
                     chars.remove(i);
-                    count++;
-                } else {
-                    for(int j : lists.keySet()) {
-                        if(!lists.get(j).contains(chars.get(i)) && lists.get(j).size() < 3) {
-                            lists.get(j).add(chars.get(i));
-                            chars.remove(i);
-                        } else if(chars.size() >= 3 && isEverythingMin()) {
-                            lists.put(count, new ArrayList<>());
-                            lists.get(count).add(chars.get(i));
-                            chars.remove(i);
-                            count++;
-                        }
-                    }
+                } else if (chars.size() >= 3 && isEverythingMin()) {
+                    lists.put(lists.size(), new ArrayList<>());
+                    lists.get(lists.size() - 1).add(chars.get(i));
+                    chars.remove(i);
                 }
             }
-
-            if(chars.size() < 3) break;
         }
 
-        for(int k : lists.keySet()) {
-            for(int l : chars.keySet()) {
-                if(!lists.get(k).contains(chars.get(l))) {
+
+        for (int k : lists.keySet()) {
+            for (int l : chars.keySet()) {
+                if (!lists.get(k).contains(chars.get(l))) {
                     lists.get(k).add(chars.get(l));
                     chars.remove(l);
                 }
             }
         }
 
-        if(chars.size() > 0) {
-            System.out.println("Error");
-        } else {
-            System.out.println("Success");
-        }
+        System.out.println((chars.size() > 0 ? "Error" : "Success"));
     }
 
     private static boolean isEverythingMin() {
